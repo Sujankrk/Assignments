ef81a1956ca29d59e4d4ecbdda503913e0a43e1a
Sujankrk <124369980+Sujankrk@users.noreply.github.com>
2023-02-07 14:30:05 +0545

    Add files via upload

--- CalculateSIAndCI.java
+++ CalculateSIAndCI.java
@@ -3,25 +3,26 @@ public class CalculateSIAndCI {
         Calculate Interest1= new Calculate();
         Interest1.calculateSimpleInterest(30000,3, 3.3f);
 
-        //Interest1.calculateCompoundInterest();
+        Interest1.calculateCompoundInterest();
+    }
+}
+class Calculate {
+    void calculateSimpleInterest(int p, int n, float r) {
+        float si = (p * n * r) / 100;
+        System.out.println("The Simple Interest is: " + si);
+
+    }
+
+    void calculateCompoundInterest() {
+        int p = 30000;
+        float r = 3f; //rate of interest
+        int n = 3;
+        int t = 12; //time in years
+        double ci = p * (1 + 3 / 3) ^ (3 * 12);
+        System.out.println("The Compound Interest is: " + ci);
+
 
     }
 }
-class Calculate{
-    void calculateSimpleInterest(int p, int n, float r){
-        float si= (p*n*r)/100;
-        System.out.println("The Simple Interest is: " +si);
-
-    }
-   /* void calculateCompoundInterest (){
-        int p=30000;
-        int rateOfInterest r= 3
-        int t=12;
-        double ci= 30000*(1+3.3f/3)^(3*12);
-        System.out.println("The Compound Interest is: "+ci);
-
-    */
-
-    }
 
 
